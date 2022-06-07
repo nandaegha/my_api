ff47dbe8f129164060fc7ef7fde4d8ee2d0c7bc9
FlutLab.io <>
2022-06-07 03:57:34 +0000

    api3

--- lib/api-manager.dart
+++ lib/api-manager.dart
@@ -3,29 +3,13 @@ import 'package:http/http.dart';
 import 'package:api/soccermodel.dart';
 
 class SoccerApi {
-  //now let's set our variables
-  //first : let's add the endpoint URL
-  // we will get all the data from api-sport.io
-  // we will just change our endpoint
-  //the null means that the match didn't started yet
-  //let's fix that
   final String apiUrl = "https://v3.football.api-sports.io/fixtures?live=all";
-  //In our tutorial we will only see how to get the live matches
-  //make sure to read the api documentation to be ables too understand it
 
-  // you will find your api key in your dashboard
-  //so create your account it's free
-  //Now let's add the headers
   static const headers = {
     'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-    //Always make sure to check the api key and the limit of a request in a free api
     'x-rapidapi-key': "b05754665c3577b9973045b768d96413"
   };
 
-  //Now we will create our method
-  //but before this we need to create our model
-
-  //Now we finished with our Model
   Future<List<SoccerMatch>> getAllMatches() async {
     Response res = await get(apiUrl, headers: headers);
     var body;
